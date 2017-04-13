493f1ed1748106bab2349b08daa3b4b07a39b697
andreygolubkow <andreygolubkow@gmail.com>
2017-04-13 13:09:44 +0700

    Начал делать главную форму(классы)

--- Model/Project/IssueJournal.cs
+++ Model/Project/IssueJournal.cs
@@ -1,10 +1,26 @@
 ﻿namespace Model.Project
 {
     using System;
+    using System.Collections.Generic;
+
+    using Model.Employee;
+    using Model.Professions;
 
     [Serializable]
     public class IssueJournal
     {
-        
+
+        public IssueJournal()
+        {
+            
+        }
+
+        public IEmployee Employee { get; set; }
+
+        public IList<IProfession> Professions { get; set; }
+
+        public IList<Resource> Resources { get; set; }
+
+
     }
 }
---
+++ Model/Project/Resource.cs
@@ -0,0 +1,38 @@
+﻿namespace Model.Project
+{
+    using System;
+
+    using Model.Employee;
+    using Model.Equipments;
+
+    /// <summary>
+    /// Вещь которая выдается.
+    /// </summary>
+    public class Resource
+    {
+        private double _count;
+
+        public IEquipment Equipment { get; set; }
+
+        public double Count
+        {
+            get
+            {
+                return _count;
+            }
+            set
+            {
+                if ( value > 0 )
+                {
+                    _count = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Количество должно быть больше нуля.");
+                }
+            }
+        }
+
+        public string Additional { get; set; }
+    }
+}
